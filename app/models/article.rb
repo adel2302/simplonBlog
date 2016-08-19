2957cb309eef80effd1f172f0459ddcb6c526e5f
adel2302 <a.chouanine@hotmail.fr>
2016-08-19 22:29:36 +0200

    Added act as votable

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  acts_as_votable
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :comments
