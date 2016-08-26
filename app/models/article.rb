9f340f1413e6213e1de261244739787ffa7ae805
adel2302 <a.chouanine@hotmail.fr>
2016-08-26 22:26:39 +0200

    added img background and fixed issues

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   acts_as_votable
   acts_as_taggable
-  has_attached_file :image, styles: { large: "900x300", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: "900x300", medium: "330x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :comments
   belongs_to :user
